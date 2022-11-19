4c0008431467e08e066984afd4fa254d86253640
Eduardo Alessandro Bonelli Junior <eduardo.abjr@gmail.com>
2022-11-19 12:44:51 -0300

    introdução da logica

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
-const calcBtn = document.querySelector('#btn');
 
-calcBtn.addEventListener("click", () => {
-    console.log('teste');
-});
+
+
+
+
+function calcImc(){
+    let peso = document.querySelector("#peso").value;
+    let altura = document.querySelector("#altura").value;
+    let imc = peso/(altura*altura);
+    let text = document.getElementById('imcResult');
+    let imcHTML = '<p> Seu imc é: </p>'+ imc.toFixed(2);
+    text.innerHTML = imcHTML;
+    console.log(imc);
+};
+
+
