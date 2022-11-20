14c386389da87c337a409b223e22d694397fd03a
Eduardo Bonelli jr <eduardo.abjr@gmail.com>
2022-11-19 21:51:14 -0300

    Inclusão de mensagem do imc, e melhorias gerais

--- script.js
+++ script.js
@@ -9,9 +9,37 @@ function calcImc(){
     let altura = document.querySelector("#altura").value;
     let imc = peso/(altura*altura);
     let text = document.getElementById('imcResult');
-    let imcHTML = '<p> Seu imc é: </p>'+ imc.toFixed(2);
-    text.innerHTML = imcHTML;
-    console.log(imc);
+    
+    if (imc < 18.5){
+        let imcHTML = '<p> Seu imc é: '+ imc.toFixed(2) + '</p><p>Sua classificação é abaixo do peso normal </p>';   
+        text.innerHTML = imcHTML;    
+    }
+    else if(imc >= 18.5 && imc < 25 ){
+        let imcHTML = '<p> Seu imc é: '+ imc.toFixed(2) + '</p><p>Sua classificação é peso normal </p>';
+        text.innerHTML = imcHTML;    
+    }
+
+    else if(imc >= 25 && imc < 30 ){
+        let imcHTML = '<p> Seu imc é: '+ imc.toFixed(2) + '</p><p>Sua classificação é excesso de peso </p>';
+        text.innerHTML = imcHTML;
+    }
+    
+    else if(imc >=30 && imc < 35 ){
+        let imcHTML = '<p> Seu imc é: '+ imc.toFixed(2) + '</p><p>Sua classificação é obesidade classe 1 </p>';
+        text.innerHTML = imcHTML;    
+    }
+    
+    else if(imc >= 35 && imc < 40 ){
+        let imcHTML = '<p> Seu imc é: '+ imc.toFixed(2) + '</p><p>Sua classificação é obesidade classe 2 </p>';
+        text.innerHTML = imcHTML;
+    }
+    else if(imc >40){
+        let imcHTML = '<p> Seu imc é: '+ imc.toFixed(2) + '</p><p>Sua classificação é obesidade classe 3 </p>';    
+        text.innerHTML = imcHTML;
+    }
+    
+    
+    
 };
 
 
